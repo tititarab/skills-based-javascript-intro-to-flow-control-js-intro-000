bfc934c6876d2a967371fe8deb76c65fada040fa
Tiffany Tarab <tiffany.tarab@gmail.com>
2017-09-01 13:54:58 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -15,9 +15,9 @@ if (age>=13 && age<=19) {
 function ageChecker(age) {
 if (age>=13 && age<=19) {
   return "You are a teenager!"
-} else if ("age"<=12) {
+} else if (age<=12) {
   return "You are a kid"
-} else if ("age">20) {
+} else if (age>20) {
   return "You are a grownup"
 }
 }
