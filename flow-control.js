4f054778e6109c69f2d048fd107087166c36b70d
Tiffany Tarab <tiffany.tarab@gmail.com>
2017-09-01 13:50:48 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,10 @@ return age>13 && age<19 ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
 switch (age) {
-  case "age":
-  return "You are a teenager"
+  case age>=13 && age<=19:
+  return "You are a teenager";
+  break;
+  default:
+  return "You are not a teenager";
 }
 }
